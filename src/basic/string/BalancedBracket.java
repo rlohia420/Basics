b22ad31607dabf779e3ade5f6b8228b916d33c78
rlohia420 <lohianeha1988@gmail.com>
2020-07-30 12:50:03 +0530

    changes to add the balanced bracket program[master]

--- src/basic/string/BalancedBracket.java
+++ src/basic/string/BalancedBracket.java
@@ -1,11 +1,52 @@
 package basic.string;
 
 import java.io.IOException;
+import java.util.Stack;
 
 public class BalancedBracket {
 	public static void main(String args[]) throws IOException {
-		isBalance("(iuuyuy)");
+		System.out.println(isBalance("[NEHA{LOHIA}PRATEEK]"));
 	}
-	private static void isBalance(String string) {
+	private static boolean isBalance(String string) {
+		{
+			Stack<Character> stack = new Stack<Character>();
+		    String newExp = string;
+		    for (int i = 0; i < newExp.length(); i++)
+		    { 
+		      char ch = newExp.charAt(i);
+		      if (ch == '(' || ch == '[' || ch == '{')
+		        stack.push(ch);
+		      else if (ch == ')'|| ch == ']' || ch == '}')
+		      {
+		    	  Character ch1 = null;
+		    	  if(ch==')') {
+		    		  ch1='(';
+		    	  }else if(ch=='}') {
+		    		  ch1='{';
+		    	  }else if(ch==']') {
+		    		  ch1='[';
+		    	  }
+		        if(stack.isEmpty())
+		        {  
+		          return false;
+		        }
+		        else if(stack.pop() == ch1)
+		        { 
+		        	continue;
+		        } else {
+		        	return false;
+		        }
+
+		      }            
+		    }
+		    if (stack.isEmpty())
+		    {
+		      return true;
+		    }
+		    else
+		    {
+		      return false;
+		    }
+		}
 	}
 }
